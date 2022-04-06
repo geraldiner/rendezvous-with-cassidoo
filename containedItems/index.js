49e2a9b1464bb6a5441667f9995b0f2d2bb9324a
Geraldine <hello@geraldiner.com>
2022-04-06 16:30:46 -0400

    initial commit with first 3 problems

---
+++ containedItems/index.js
@@ -0,0 +1,10 @@
+/**
+ * Given a string that represents items as asterisks (*) and compartment walls as pipes (|), a start index, and an end index, return the number of items in a closed compartment.
+ *
+ * Example:
+ * let str = '|**|*|*'
+ *
+ * containedItems(str, 0, 5) => 2
+ * containedItems(str, 0, 6) => 3
+ * scontainedItems(str, 1, 7) => 1
+ */
---
+++ smallestTimeInterval/index.js
@@ -0,0 +1,8 @@
+/**
+ * Given a list of times in a 24-hour period, return the smallest interval between two times in the list. Hint: you can do this in O(n) time! Assume the list is unsorted.
+ *
+ * Example:
+ *
+ * $ smallestTimeInterval(['01:00', '08:15', '11:30', '13:45', '14:10', '20:05'])
+ * $ '25 minutes'
+ */
