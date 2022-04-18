6fbd617b0de68ed4deffc6e5dab56faca55a0906
Geraldine <hello@geraldiner.com>
2022-04-18 09:15:46 -0400

    add new weekly problem

---
+++ largestSubarraySum/index.js
@@ -0,0 +1,7 @@
+/**
+ * Given an unsorted array of integers and a number n, find the subarray of length n that has the largest sum.
+ *
+ * Example:
+ * largestSubarraySum([3,1,4,1,5,9,2,6], 3)
+ * [9, 2, 6]
+ *  */
