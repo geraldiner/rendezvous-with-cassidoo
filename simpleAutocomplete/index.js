e784a9c8f972f3917c434a3f56d22f3697fd1e33
Geraldine <hello@geraldiner.com>
2022-05-11 10:39:16 -0400

    add some questions from the last 2 weeks

---
+++ simpleAutocomplete/index.js
@@ -0,0 +1,15 @@
+/**
+ * Implement a simple version of autocomplete, where given an input string s and a dictionary of words dict, return the word(s) in dict that partially match s (or an empty string if nothing matches).
+ *
+ * Example:
+ * let dict = ['apple', 'banana', 'cranberry', 'strawberry']
+ *
+ * simpleAutocomplete('app')
+ * ['apple']
+ *
+ * simpleAutocomplete('berry')
+ * ['cranberry', 'strawberry']
+ *
+ * simpleAutocomplete('fart')
+ * []
+ */
---
+++ whoOwes/index.js
@@ -0,0 +1,18 @@
+/**
+ * You went on a vacation with friends. Each of you paid for certain meals on the trip for the group. Write a function that determines who owes money to whom so that everyone pays equally.
+ *
+ * Example:
+ *
+ * let receipts = [
+ *  { name: 'Ximena', paid: 45 },
+ *  { name: 'Clara', paid: 130 },
+ *  { name: 'Ximena', paid: 100 },
+ *  { name: 'Cassidy', paid: 140 },
+ *  { name: 'Cassidy', paid: 76 },
+ *  { name: 'Clara', paid: 29 },
+ *  { name: 'Ximena', paid: 20 },
+ * ]
+ *
+ * whoOwes(receipts)
+ * 'Clara owes Cassidy $19, Ximena owes Cassidy $17'
+ *  */
