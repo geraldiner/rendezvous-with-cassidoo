a60b1b3b76db62779479dcf5e6207bf3453dab0d
Geraldine <hello@geraldiner.com>
2022-06-16 20:14:51 -0400

    add weekly problem

---
+++ longText/index.js
@@ -0,0 +1,10 @@
+/**
+ * Create a loooong teeeext generator that takes in a string and an integer n, and multiplies the vowels in the string by n.
+ *
+ * Example:
+ * longText('hello world', 3)
+ * 'heeellooo wooorld'
+ *
+ * longText('lol', 10)
+ * 'looooooooool'
+ */
