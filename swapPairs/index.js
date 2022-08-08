9b846bd996e05a87c7e51e5f157da36bfebdef75
Geraldine <hello@geraldiner.com>
2022-08-08 18:45:22 -0400

    add weekly question

---
+++ swapPairs/index.js
@@ -0,0 +1,10 @@
+/**
+ * Given a list, swap every two adjacent nodes. Something to think about: How would your code change if this were a linked list, versus an array?
+ *
+ * Example:
+ * swapPairs([1,2,3,4])
+ * [2,1,4,3]
+ *
+ * swapPairs([])
+ * []
+ */
